be9d1f8867a10bbd6a15b4543e776349d9b3e5b2
tmarlok <tmarlok@gmail.com>
2021-10-12 18:20:44 +0200

    ci: testing smoke testing and auto-rollback in stage

---
+++ tests/smoke/test_smoke.py
@@ -0,0 +1,16 @@
+import unittest
+import requests
+import os
+
+
+class SmokeTests(unittest.TestCase):
+
+    def test_admin_endpoint(self):
+        self.assertEqual(requests.get(f'{os.getenv("WEB_URL")}/admin').status_code, 201)  # deliberately fail
+
+    def test_rsvp_endpoint(self):
+        self.assertEqual(requests.get(f'{os.getenv("WEB_URL")}/rsvp').status_code, 404)
+
+
+if __name__ == '__main__':
+    unittest.main()
