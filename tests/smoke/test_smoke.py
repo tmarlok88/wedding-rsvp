cc7177c48b4216a339ab011369771759e2e18e99
tmarlok <tmarlok@gmail.com>
2021-10-13 19:27:08 +0200

    ci: smoke testing and auto-rollback in production

--- tests/smoke/test_smoke.py
+++ tests/smoke/test_smoke.py
@@ -6,7 +6,7 @@ import os
 class SmokeTests(unittest.TestCase):
 
     def test_admin_endpoint(self):
-        self.assertEqual(requests.get(f'{os.getenv("WEB_URL")}/admin').status_code, 201)  # deliberately fail
+        self.assertEqual(requests.get(f'{os.getenv("WEB_URL")}/admin').status_code, 200)
 
     def test_rsvp_endpoint(self):
         self.assertEqual(requests.get(f'{os.getenv("WEB_URL")}/rsvp').status_code, 404)
